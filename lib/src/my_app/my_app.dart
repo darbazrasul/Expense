2b6916eed74abb17d4294ee22b55b49df924aeb9
darbaz dev <110346010+darbazrasul@users.noreply.github.com>
2024-01-03 15:39:19 +0300

    change app style using by theme

--- lib/src/my_app/my_app.dart
+++ lib/src/my_app/my_app.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 import '../screen/expenses.dart';
 
+var kcolortheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 159),
+);
+
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
@@ -9,7 +13,26 @@ class MyWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData(useMaterial3: true).copyWith(
+          colorScheme: kcolortheme,
+          appBarTheme: AppBarTheme(
+            backgroundColor: const Color.fromARGB(255, 96, 59, 159),
+            foregroundColor: kcolortheme.primaryContainer,
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: kcolortheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+            backgroundColor: kcolortheme.primaryContainer,
+          )),
+          textTheme: ThemeData().textTheme.copyWith(
+                  titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: kcolortheme.primaryContainer,
+                fontSize: 14,
+              ))),
       home: Expenses(),
     );
   }
